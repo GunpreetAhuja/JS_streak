5db33010af5130968a766a6e6d9a51f19c2e0c1d
GunpreetAhuja <gunpreetahuja0528@gmail.com>
2014-07-25 21:40:53 +0530

    First JS file (codecademy)

---
+++ JS/Simplest_interaction.js
@@ -0,0 +1,31 @@
+confirm("Are you ready to play?");
+var age = prompt("What's ur age?");
+if( age < 13 )
+{
+    console.log("Allowed");
+}
+else
+{
+    console.log("Go on!");
+}
+
+console.log("You are at a Justin Bieber concert, and you hear this lyric 'Lace my shoes off, start racing.'");
+console.log("Suddenly, Bieber stops and says, 'Who wants to race me?'");
+var userAnswer = prompt("Do you want to race Bieber on stage?");
+if(userAnswer == "yes")
+{
+    console.log("You and Bieber start racing. It's neck and neck! You win by a shoelace!");
+}
+else
+{
+    console.log("Oh no! Bieber shakes his head and sings 'I set a pace, so I can race without pacing.'");
+}
+var feedback = prompt("Rate the game out of 10");
+if(feedback > 8)
+{
+    console.log("Thank you! We should race at the next concert!");
+}
+else
+{
+    console.log("I'll keep practicing coding and racing.");
+}
